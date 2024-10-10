47c77f5c00b8832d9e45a2bb2419306b5f7aaa4c
OrMoshe <ormoshe2468@gmail.com>
2024-10-10 17:09:48 +0300

    send to server

--- content.js
+++ content.js
@@ -1,4 +1,22 @@
 
+const sendToServer = async(data) => {
+    try {
+        const request = new Request("http://127.0.0.1:8000/", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'}, // Make sure to set your headers correctly
+            body: JSON.stringify( data )
+        });
+        const response = await fetch(request);
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
+        const res = await response.json();
+        console.log(res);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 const getItemData = async(body) => {
     return new Promise((resolve, reject) => {
         let details = {};
@@ -26,20 +44,24 @@ const getItemData = async(body) => {
 
 const getItems = async() => {
     const items = document.querySelectorAll('[data-nagish="feed-item-list-box"]');
-    data = [];
-    items.forEach(async(item) => {
+    //forEach does not wait for promises to resolve before moving to the next iteration 
+
+    //instead of using forEach, map us used to create a new array of promises. 
+    const dataPromises = Array.from(items).map(async(item) => {
         const url = item.querySelector('a').href;
-        const itemId = url.split('/')[5].split('?')[0];
+        const itemId = url.split('/').pop().split('?')[0];
         const htmlText = (await (await fetch(url)).text()); // html as text
         const body = new DOMParser().parseFromString(htmlText, 'text/html').body;
         const baseData = {link: url, type: item.dataset.testid, innerText: item.innerText};
         const additionalData = await getItemData(body);
-        data.push({id: itemId,
+        /*data.push({id: itemId,
             ...baseData,
             ...additionalData
-        });
+        });*/
+        return { id: itemId, ...baseData, ...additionalData };
     });
-    console.log(data);
+    const data = await Promise.all(dataPromises);
+    sendToServer(data);
 };
 
 getItems();
